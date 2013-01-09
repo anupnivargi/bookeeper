de8aae42bc843d17df6947ecf4ec016dd8201f06
Anup Nivargi <anivargi@redhat.com>
2013-01-09 17:24:01 +0530

    Integrated with Google Books API

---
+++ public/js/application.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+  $("#entry-isbn").focus();
+
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+
+  $("#entry-isbn").keyup(function(){
+    if($(this).val().length == 13){
+      var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + $(this).val();
+      $.ajax({ 
+        url : url,
+        dataType: "json",
+        success: function(data, textStatus, xhr){
+          var totalItems = data.totalItems;
+          if(totalItems > 0){
+            var items = data.items;
+            var item = items[0];
+            var volumeInfo = item.volumeInfo;
+            var title = volumeInfo.title;
+            var subtitle = volumeInfo.subtitle;
+            if(subtitle){
+              $("#entry-name").val(title + ":" + subtitle);
+            }else{
+              $("#entry-name").val(title);
+            }
+            $("#entry-form").submit();
+          }else{
+            alert("No match found");
+            $("#entry-isbn").val("");
+          }
+        }
+      });
+    }
+
+  }); 
+});
+
